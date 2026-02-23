c98713e8d1161e3a06455dbfefb514cee03f4351
coden13 <Bennet.Warmuth@gmx.de>
2026-02-23 20:23:39 +0100

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bennetlolll & ProfieAngel'
+description 'Bennet RP System'
+version '1.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/Foto.png',
+    'html/music.mp3'
+}
+
+client_scripts {'client.lua'}
+server_scripts {'server.lua'}
